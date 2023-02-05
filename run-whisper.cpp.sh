02d84642209311eec50d5fe557902605eb2bbcac
Jordi Mas <jmas@softcatala.org>
2023-02-05 08:04:29 +0100

    Initial version

---
+++ run-whisper.cpp.sh
@@ -0,0 +1,5 @@
+#cp ../transcribe-service/whisper.cpp/main .
+#cp /home/jordi/sc/transcribe/transcribe-service/whisper.cpp/sc-models/* models
+ffmpeg -i 15GdH5.mp3 -ar 16000 -ac 1 -c:a pcm_s16le 15GdH5.wav -y 
+./main  --threads 14  -m models/ggml-medium.bin -f 15GdH5.wav -l ca -otxt  -osrt
+
---
+++ run-whisper.sh
@@ -0,0 +1,2 @@
+# pip install --upgrade --no-deps --force-reinstall git+https://github.com/openai/whisper.git
+whisper 15GdH9-curt.mp3 --model medium --language ca --output_dir 15GdH9-curt/
